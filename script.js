4a31a6bcef7b83ca54fd4928246059868b96aa82
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-01-25 23:13:06 +0700

    Added section section and made it responsive...ish

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 anime({
 	targets: ".amazing path",
 	translateX: 70,
-	delay: anime.stagger(50), // increase delay by 100ms for each elements.
+	delay: anime.stagger(100),
+	 // increase delay by 100ms for each elements.
 });
 
 console.log("hello");
