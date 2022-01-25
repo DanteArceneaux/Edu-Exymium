5a2922bf66c5c898a42649fb08116112f3da0859
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-01-25 18:24:47 +0700

    Made nav and main section responsive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 anime({
-    targets: 'path',
-    translateX: 270,
-    delay: anime.stagger(50) // increase delay by 100ms for each elements.
-  });
+	targets: ".amazing path",
+	translateX: 70,
+	delay: anime.stagger(50), // increase delay by 100ms for each elements.
+});
 
- 
+console.log("hello");
