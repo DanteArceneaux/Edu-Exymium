a9fbc66d161f31222df6b00a9b559229c7874128
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-01-27 00:31:20 +0700

    Made the scroll-up animation button work!

--- script.js
+++ script.js
@@ -6,13 +6,33 @@ anime({
 });
 
 let scrollDown = document.querySelector(".scroll-down");
+let scrollUp = document.getElementById("scroll-up");
+let secondSection = document.getElementById("second-section");
 
 scrollDown.addEventListener("click", function () {
 	window.scrollBy({ top: 1000, left: 0, behavior: "smooth" });
+});
+
+scrollUp.addEventListener("click", function () {
+	window.scrollBy({ top: -1000, left: 0, behavior: "smooth" });
 });
 
 function sendEmail() {
 	window.location = "mailto:dantearcene@gmail.com";
 }
 
-let scrollUp = document.querySelector(".scroll-up");
+var lastScroll = 0;
+$(window).scroll(function (event) {
+	//Sets the current scroll position
+	var st = $(this).scrollTop();
+	//Determines up-or-down scrolling
+	if (st < 100) {
+		//secondaryNav disappears when scrolled down
+		$(".scroll-up").css("display", "none");
+	} else {
+		//secondaryNav disappears when scrolled up
+		$(".scroll-up").css("display", "block");
+	}
+	//Updates scroll position
+	lastScroll = st;
+});
